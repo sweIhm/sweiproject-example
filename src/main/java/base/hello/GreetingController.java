ad164f65099ce63bb98eeb7bbec4c81e8b41f64f
Gudrun Socher <gudrun.socher@hm.edu>
2017-12-01 17:48:47 +0100

    String constant all upper case

--- src/main/java/base/hello/GreetingController.java
+++ src/main/java/base/hello/GreetingController.java
@@ -8,12 +8,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GreetingController {
 
-    private static final String Template = "Hello, %s!";
+    private static final String TEMPLATE = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping("/greeting")
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
         return new Greeting(counter.incrementAndGet(),
-                            String.format(Template, name));
+                            String.format(TEMPLATE, name));
     }
 }
